ba21488890b2df9a82934b735ae8b8b6099fab8b
Hasan Güngör <hasangungr1@gmail.com>
2024-08-14 14:28:19 +0300

    Sequilaze import

--- utility/database.js
+++ utility/database.js
@@ -2,7 +2,10 @@ const mysql = require('mysql2');
 
 
 const connection = mysql.createConnection({
-
+    host: 'localhost',
+    user: 'root',
+    database: 'node-app',
+    password: '12345678'
 });
 
 
